f036db2fbfa6358b170595d8d15ddb5c29099e07
Patsuev Sergei <s.patsuev@haulmont.com>
2022-03-09 09:54:59 +0400

    initial commit

---
+++ src/main/java/com/luxoft/springioc/lab3/model/RegistrationManager.java
@@ -0,0 +1,20 @@
+package com.luxoft.springioc.lab3.model;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Service;
+
+@Service("registrationManager")
+public class RegistrationManager implements ApplicationContextAware {
+	private ApplicationContext applicationContext;
+
+	public ApplicationContext getApplicationContext() {
+		return applicationContext;
+	}
+
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+
+}
